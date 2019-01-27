ba9fad88ff5441f42affec79a742eda381085f71
Evan Greco <evan.greco@legitscript.com>
2019-01-27 15:19:57 -0800

    WIP, add apollo-server-express next

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server');
+const { GraphQLScalarType } = require('graphql');
 
 const typeDefs = `
+  scalar DateTime
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -16,6 +19,8 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
+    created: DateTime!
   }
 
   input PostPhotoInput {
@@ -29,6 +34,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   type Query {
@@ -53,22 +59,33 @@ var photos = [
     "name": "Dropping the Heart Chute",
     "description": "The heart chute is one of my favorite chutes",
     "category": "ACTION",
-    "githubUser": "gPlake"
+    "githubUser": "gPlake",
+    "created": "3-28-1977"
   },
   {
     "id": "2",
     "name": "Enjoying the sunshine",
     "category": "SELFIE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "1-2-1985"
   },
   {
     "id": "3",
     "name": "Gunbarrel 25",
     "description": "25 laps on gunbarrel today",
     "category": "LANDSCAPE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "2018-04-15T19:09:57.308Z"
   }
 ];
+
+var tags = [
+  {"photoID": "1", "userID": "gPlake"},
+  {"photoID": "2", "userID": "sSchmidt"},
+  {"photoID": "2", "userID": "mHattrup"},
+  {"photoID": "3", "userID": "gPlake"}
+];
+
 var _id = 0;
 
 const resolvers = {
@@ -81,7 +98,8 @@ const resolvers = {
     postPhoto(parent, args) {
       var newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        created: new Date()
       }
       photos.push(newPhoto);
       return newPhoto;
@@ -90,15 +108,36 @@ const resolvers = {
 
   Photo: {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+
     postedBy: parent => {
       return users.find(u => u.githubLogin == parent.githubUser)
+    },
+
+    taggedUsers: (parent) => {
+      return tags.filter(tag => tag.photoID === parent.id)
+          .map(tag => tag.userID)
+          .map(userID => users.find(u  => u.githubLogin === userID))
     }
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser == parent.githubLogin);
+    },
+
+    inPhotos: parent => {
+      return tags.filter(tag => tag.userID == parent.id)
+                            .map(tag => tag.photoID)
+                            .map(photoID => photos.find(p => p.id === photoID))
     }
-  }
+  },
+
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A valid date time value.',
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 }
 
 const server = new ApolloServer({
